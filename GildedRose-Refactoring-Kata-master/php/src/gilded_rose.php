c5fd77366c8c2d5834a611c2fc989d5fdcf8e963
Angel <angel.alonso@students.salleurl.edu>
2019-01-21 22:34:34 +0100

    Refactoring del código original incluyendo polimorfismo

--- GildedRose-Refactoring-Kata-master/php/src/gilded_rose.php
+++ GildedRose-Refactoring-Kata-master/php/src/gilded_rose.php
@@ -2,78 +2,144 @@
 
 class GildedRose {
 
-    private $items;
+	private $items;
 
-    function __construct($items) {
-        $this->items = $items;
-    }
+	function __construct($items) {
+		$this->items = $items;
+	}
 
-    function update_quality() {
-        foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-            
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sell_in = $item->sell_in - 1;
-            }
-            
-            if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
-            }
-        }
-    }
+	function updateQuality() {
+		foreach ($this->items as $item) {
+			$item->updateQualityItem();
+		}
+	}
+	function updateSellIn() {
+		foreach ($this->items as $item) {
+			$item->updateSellInItem();
+		}
+	}
 }
 
 class Item {
 
-    public $name;
-    public $sell_in;
-    public $quality;
+	public $name;
+	public $sell_in;
+	public $quality;
 
-    function __construct($name, $sell_in, $quality) {
-        $this->name = $name;
-        $this->sell_in = $sell_in;
-        $this->quality = $quality;
-    }
+	function __construct($name, $sell_in, $quality) {
+		$this->name = $name;
+		$this->sell_in = $sell_in;
+		$this->quality = $quality;
+	}
 
-    public function __toString() {
-        return "{$this->name}, {$this->sell_in}, {$this->quality}";
-    }
+	public function __toString() {
+		return "{$this->name}, {$this->sell_in}, {$this->quality}";
+	}
 
 }
 
+final class StandardItem extends Item {
+
+	const QUALITYLIMIT = 0;
+
+	public function updateQualityItem() {
+		if (isOutOfDate($this->sell_in)) {
+			$this->quality = $this->quality - 2;
+		} else {
+			$this->quality = $this->quality - 1;
+		}
+		if ($this->quality < self::QUALITYLIMIT) {
+			$this->quality = self::QUALITYLIMIT;
+		}
+
+	}
+	public function updateSellInItem() {
+		$this->sell_in = $this->sell_in - 1;
+
+	}
+
+}
+
+final class AgedBrieItem extends Item {
+
+	const QUALITYLIMIT = 50;
+
+	public function updateQualityItem() {
+		if (isOutOfDate($this->sell_in)) {
+			$this->quality = $this->quality + 2;
+		} else {
+			$this->quality = $this->quality + 1;
+		}
+		if ($this->quality > self::QUALITYLIMIT) {
+			$this->quality = self::QUALITYLIMIT;
+		}
+
+	}
+	public function updateSellInItem() {
+		$this->sell_in = $this->sell_in - 1;
+
+	}
+
+}
+
+final class SulfurasItem extends Item {
+
+	const QUALITYLIMIT = 80;
+
+	public function updateQualityItem() {
+
+		$this->quality = self::QUALITYLIMIT;
+
+	}
+	public function updateSellInItem() {
+		$this->sell_in = $this->sell_in;
+
+	}
+
+}
+
+final class BackstageItem extends Item {
+
+	const QUALITYLIMIT = 50;
+	const ALMOSTLASTDAYS = 10;
+	const LASTDAYS = 5;
+	const CONCERTDAY = 0;
+
+	public function updateQualityItem() {
+
+		if ($this->sell_in > self::ALMOSTLASTDAYS) {
+			$this->quality = $this->quality + 1;
+
+		} elseif ($this->sell_in <= self::ALMOSTLASTDAYS && $this->sell_in > self::LASTDAYS) {
+			$this->quality = $this->quality + 2;
+
+		} elseif ($this->sell_in <= self::LASTDAYS && $this->sell_in >= self::CONCERTDAY) {
+			$this->quality = $this->quality + 3;
+
+		}
+
+		if ($this->sell_in < self::CONCERTDAY) {
+			$this->quality = 0;
+
+		}
+
+		if ($this->quality > self::QUALITYLIMIT) {
+			$this->quality = self::QUALITYLIMIT;
+
+		}
+
+	}
+	public function updateSellInItem() {
+		$this->sell_in = $this->sell_in - 1;
+
+	}
+
+}
+
+function isOutOfDate($sellDate) {
+	if ($sellDate <= 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
--- GildedRose-Refactoring-Kata-master/php/src/texttest_fixture.php
+++ GildedRose-Refactoring-Kata-master/php/src/texttest_fixture.php
@@ -5,31 +5,35 @@ require_once 'gilded_rose.php';
 echo "OMGHAI!\n";
 
 $items = array(
-    new Item('+5 Dexterity Vest', 10, 20),
-    new Item('Aged Brie', 2, 0),
-    new Item('Elixir of the Mongoose', 5, 7),
-    new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-    new Item('Sulfuras, Hand of Ragnaros', -1, 80),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
-    // this conjured item does not work properly yet
-    new Item('Conjured Mana Cake', 3, 6)
+	new StandardItem('+5 Dexterity Vest', 10, 20),
+	new AgedBrieItem('Aged Brie', 2, 0),
+	new StandardItem('Elixir of the Mongoose', 5, 7),
+	new SulfurasItem('Sulfuras, Hand of Ragnaros', 0, 80),
+	new SulfurasItem('Sulfuras, Hand of Ragnaros', -1, 80),
+	new BackstageItem('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+	new BackstageItem('Backstage passes to a TAFKAL80ETC concert', 10, 49),
+	new BackstageItem('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+	// this conjured item does not work properly yet
+	new StandardItem('Conjured Mana Cake', 3, 6),
 );
 
 $app = new GildedRose($items);
 
 $days = 2;
 if (count($argv) > 1) {
-    $days = (int) $argv[1];
+	$days = (int) $argv[1];
 }
 
+$prueba = new StandardItem('+5 Dexterity Vest', 10, 20);
+
 for ($i = 0; $i < $days; $i++) {
-    echo("-------- day $i --------\n");
-    echo("name, sellIn, quality\n");
-    foreach ($items as $item) {
-        echo $item . PHP_EOL;
-    }
-    echo PHP_EOL;
-    $app->update_quality();
+	echo ("-------- day $i --------\n");
+	echo ("name, sellIn, quality\n");
+	foreach ($items as $item) {
+		echo $item . PHP_EOL;
+	}
+	echo PHP_EOL;
+	$app->updateQuality();
+	$app->updateSellIn();
+
 }
